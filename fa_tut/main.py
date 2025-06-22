9351d8c322304d95c86705393859a26a76a38c7b
Emmanuele Levi <ignisvictorious@gmail.com>
2025-06-22 17:23:08 +0100

    Change params

--- fa_tut/main.py
+++ fa_tut/main.py
@@ -14,9 +14,17 @@ def read_root() -> dict[str, str]:
 
 
 @app.get(path="/items/{item_id}")
+#  Use /items/12345678
 # def read_item(item_id: int) -> dict[str, int]:
+#  Use /items/12345678?q=erre
 def read_item(item_id: int, q: str) -> dict[str, int | str]:
     return {"item_id": item_id, "query": q}
+
+
+#  use ? / & to change the values: /products/?skip=9 - /products/?skip=11&limit=9
+@app.get(path="/products/")
+def list_products(skip: int = 0, limit: int = 10) -> dict[str, int]:
+    return {"skip": skip, "limit": limit}
 
 
 # def main() -> None:
