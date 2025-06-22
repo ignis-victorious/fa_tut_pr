2277cbea313924ff46547a3d31a69eda95499414
Emmanuele Levi <ignisvictorious@gmail.com>
2025-06-22 17:09:38 +0100

    add toot

--- fa_tut/main.py
+++ fa_tut/main.py
@@ -1,6 +1,21 @@
-def main() -> None:
-    print("Hello from fa-tut-pr!")
+#  ______________________
+#  Import LIBRARIES
+from fastapi import FastAPI
+#  Import FILES
+#  ______________________
 
 
-if __name__ == "__main__":
-    main()
+app: FastAPI = FastAPI()
+
+
+@app.get(path="/")
+def read_root() -> dict[str, str]:
+    return {"Hello": "world"}
+
+
+# def main() -> None:
+#     print("Hello from fa-tut-pr!")
+
+
+# if __name__ == "__main__":
+#     main()
